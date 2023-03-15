430465233346a2d1af05cd1581d58e17098da745
LaurieG28 <laurie.gazelle@gmail.com>
2023-03-15 19:03:26 +0100

    Add about component

---
+++ src/pages/Desktop_pages/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+    return (
+        <div className="About">
+            <header className="About-header">
+                piuoh
+            </header>
+        </div>
+    );
+}
+
+export default About;
